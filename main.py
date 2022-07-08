d587ea5219aeb031aaaeeefa1b14cdd60f39b0bd
jasielserra <jasiel_serra@hotmail.com>
2022-07-08 09:34:25 -0300

    Layouts for Projects and Themes.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from tkinter import *
+from tkinter import ttk
+from ttkthemes import ThemedTk
 
 class Player:
     def __init__(self):
-        self.window = Tk()
+        self.window = ThemedTk(theme="equilux")
         self.window.title("Music Player")
         self.window.resizable(0,0)
         self.window.geometry("300x400+800+300")
+        self.window.config(bg="#333333")
+
+        self.btn = ttk.Button(self.window)
+        self.btn.pack()
+
         self.window.mainloop()
 
 Player()
