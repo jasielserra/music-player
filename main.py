56c226400a893b332bd98db3b4fdee3e808810e0
jasielserra <jasiel_serra@hotmail.com>
2022-07-08 10:39:27 -0300

    Corrigindo cores and layout.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Player:
         self.window.title("Music Player")
         self.window.resizable(0,0)
         self.window.geometry("300x400+800+300")
-        self.window.config(bg="#333333")
+        self.window.config(bg="#444444")
 
         self.img_add = PhotoImage(file="assets/add.png")
         self.img_next = PhotoImage(file="assets/next.png")
@@ -17,17 +17,17 @@ class Player:
         self.img_previus = PhotoImage(file="assets/previus.png")
         self.img_remove = PhotoImage(file="assets/remove.png")
 
-        self.list = Listbox(self.window, bg="#444444", height=13)
+        self.list = Listbox(self.window, bg="#555555", height=13)
         self.list.pack(fill=X, padx=10, pady=10)
 
         self.frame = ttk.Frame(self.window)
         self.frame.pack(pady=10)
 
         self.remove = ttk.Button(self.frame, image=self.img_remove)
-        self.remove.grid(row=0, column=0)
+        self.remove.grid(row=0, column=0, padx=10)
 
         self.add = ttk.Button(self.frame, image=self.img_add)
-        self.add.grid(row=0, column=1)
+        self.add.grid(row=0, column=1, padx=10)
 
         self.frame2 = ttk.Frame(self.window)
         self.frame2.pack(pady=10)
