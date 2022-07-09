7929778a7b2d8aa737fd39b66f4cc33040326c92
Jasiel <jasiel_serra@hotmail.com>
2022-07-09 18:20:41 -0300

    Finish the Project

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Player:
         self.window = ThemedTk(theme="equilux")
         self.window.title("Music Player")
         self.window.resizable(0,0)
-        self.window.geometry("300x400+800+300")
+        self.window.geometry("300x440+800+300")
         self.window.config(bg="#444444")
 
         self.img_add = PhotoImage(file="assets/add.png")
